9f8d54892ccc4593cf91fed4ac3305b1a560c6cb
jiayuezh <jiayue.zh@outlook.com>
2024-11-07 12:10:36 +0800

    Refactor FileCard to support folder representation and dynamic image selection

--- lib/widgets/file_card.dart
+++ lib/widgets/file_card.dart
@@ -1,26 +1,56 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class FileCard extends StatelessWidget {
   final String fileName;
-  final String fileSize;
-  final DateTime createdTime;
-  final String imagePath;
+  final String? fileSize;
+  final DateTime? createdTime;
+  final bool isFolder;
+  String get fileSuffix => fileName.split('.').last;
 
-  const FileCard({
+  FileCard({
     super.key,
     required this.fileName,
     required this.fileSize,
     required this.createdTime,
-    required this.imagePath,
+    required this.isFolder,
   });
+
+  final Map<String, String> _fileTypeToImagePath = {
+    'pdf': 'assets/pdf.png',
+    'doc': 'assets/doc.png',
+    'image': 'assets/image.png',
+    'video': 'assets/video.png',
+    'folder': 'assets/folder.png',
+    'file': 'assets/file.png',
+  };
+
+  final Map<String, String> _suffixToType = {
+    'pdf': 'pdf',
+    'doc': 'doc',
+    'txt': 'doc',
+    'docx': 'doc',
+    'png': 'image',
+    'jpg': 'image',
+    'jpeg': 'image',
+    'mp4': 'video',
+    'mp3': 'video',
+    'mkv': 'video',
+    'avi': 'video',
+  };
+
+  String get imagePath {
+    if (isFolder) {
+      return _fileTypeToImagePath['folder']!;
+    }
+    final String type = _suffixToType[fileSuffix] ?? 'file';
+    return _fileTypeToImagePath[type]!;
+  }
 
   @override
   Widget build(BuildContext context) {
     final String formattedDate =
-        DateFormat('yyyy/MM/dd HH:mm:ss').format(createdTime);
+        DateFormat('yyyy/MM/dd HH:mm:ss').format(createdTime!);
 
     return Container(
       padding: const EdgeInsets.all(12.0),
@@ -31,12 +61,12 @@ class FileCard extends StatelessWidget {
       ),
       child: Row(
         children: [
-          Image.file(
-            File(imagePath),
-            height: 60,
-            width: 70,
+          Image.asset(
+            imagePath,
+            height: 35,
+            width: 45,
           ),
-          const SizedBox(width: 16),
+          const SizedBox(width: 12),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -44,18 +74,19 @@ class FileCard extends StatelessWidget {
                 Text(
                   fileName,
                   style: const TextStyle(
-                    fontSize: 18,
+                    fontSize: 16,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                const SizedBox(height: 8),
-                Text(
-                  '$formattedDate · $fileSize',
-                  style: const TextStyle(
-                    fontSize: 14,
-                    color: Colors.grey,
+                const SizedBox(height: 4),
+                if (!isFolder)
+                  Text(
+                    '$formattedDate · $fileSize',
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
                   ),
-                ),
               ],
             ),
           ),
